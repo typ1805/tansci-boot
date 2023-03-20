3657814b6034e53da9d0c9c3e5a6ee9ba9e7eba4
tanyp <742354529@qq.com>
2023-03-20 14:52:30 +0800

    初始化

---
+++ src/main/java/com/tansci/TansciBootApplication.java
@@ -0,0 +1,13 @@
+package com.tansci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TansciBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TansciBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tansci/TansciBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tansci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TansciBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
